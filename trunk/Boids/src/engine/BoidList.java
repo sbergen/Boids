302f453ccbbb97d02822c483560119b2ee831aa4
sbergen <sbergen@cd1153cc-aba8-4b8a-9c6f-7f39c3b9f640>
2009-03-14 14:44:30 +0000

    Split BoidState into ConstBoidState and BoidState

--- trunk/Boids/src/engine/BoidList.java
+++ trunk/Boids/src/engine/BoidList.java
@@ -7,19 +7,45 @@ public final class BoidList {
 	
 	/* Data members */
 	
-	ArrayList<BoidState> list;
+	private ArrayList<BoidState> list;
 	
 	/* Package methods (default visibility) */
 	
-	ArrayList<BoidState> getBoidsWithinRange(Vector position, double range) {
-		ArrayList<BoidState> neighbours = new ArrayList<BoidState>();
+	ArrayList<ConstBoidState> getBoidsWithinRange(Vector position, double range) {
+		ArrayList<ConstBoidState> neighbours = new ArrayList<ConstBoidState>();
 		
-		for (BoidState boid : list) {
+		for (ConstBoidState boid : list) {
 			if (Vector.distance(position, boid.getPosition()) <= range) {
 				neighbours.add(boid);
 			}
 		}
 		
 		return neighbours;
+	}
+	
+	/* Public methods */
+	
+	public interface BoidReader {
+		public void readBoid(ConstBoidState boid);
+	}
+	
+	public void operateList (BoidReader operator) {
+		synchronized (list) {
+			for (ConstBoidState boid : list) {
+				operator.readBoid(boid);
+			}
+		}
+	}
+	
+	public void updateBoidStates() {
+		for (ConstBoidState boid : list) {
+			boid.calculateNextMove();
+		}
+		
+		synchronized (list) {
+			for (BoidState boid : list) {
+				boid.commitNextMove();
+			}
+		}
 	}
 }
--- trunk/Boids/src/engine/BoidState.java
+++ trunk/Boids/src/engine/BoidState.java
@@ -1,20 +1,6 @@
 package engine;
 
-import vector.Vector;
-import vector.Angle;
-
-import java.util.ArrayList;
-
-public final class BoidState {
-	
-	/* Data memebers */
-	
-	private Boid      boid;
-	private PhysState state;
-	private PhysState nextState;
-	private static BoidList list;
-	
-	/* Package methods (default visibility) */
+public final class BoidState extends ConstBoidState {
 	
 	static void setList(BoidList newList) {
 		list = newList;
@@ -23,71 +9,11 @@ public final class BoidState {
 	BoidState (Boid boid, PhysState state) {
 		
 	}
-	
-	/* Public interface */
 
-	public Vector getPosition() {
-		return state.position;
-	}
-	
-	public Angle getAngle() {
-		return new Angle(state.speed);
-	}
-	
-	public Vector getSpeed() {
-		return state.speed;
-	}
-	
-	/* Package methods (default visibility) */
-	
-	void calculateNextMove() {
-		Vector force = new Vector();
-		Vector separation = new Vector();
-		Vector cohesion = new Vector();
-		Vector alignment = new Vector();
-		
-		ArrayList<BoidState> neighbours = 
-			list.getBoidsWithinRange(state.position, boid.limits.perceptionRange);
-		
-		for (BoidState neighbour : neighbours) {
-			
-			/* Separation */
-			
-			Vector diff = new Vector(neighbour.state.position);
-			diff.subtract(state.position);
-			diff.scale(1.0 / diff.length());
-			separation.add(diff);
-			
-			/* Cohesion and Alignment */
-			
-			cohesion.add(neighbour.state.position);
-			alignment.add(neighbour.state.speed);
-		}
-		
-		/* Cohesion finalization */
-		
-		cohesion.scale(1.0 / neighbours.size());
-		cohesion.subtract(state.position);
-		
-		/* Alignment finalization */
-		
-		alignment.scale(1.0 / neighbours.size());
-		alignment.subtract(state.speed);
-		
-		/* Force vector */
-		
-		force.add(separation.scale(0.2));
-		force.add(cohesion.scale(0.2));
-		force.add(alignment.scale(0.2));
-		
-		boid.attemptMove(state, nextState, force);
-	}
-	
 	void commitNextMove() {
 		PhysState tmp = state;
 		state = nextState;
 		nextState = tmp;
 	}
 	
-	/* Private helpers */
 }
