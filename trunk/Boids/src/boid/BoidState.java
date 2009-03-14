a5a009597b1e7b8494d6221f2d09e9d82dcb9388
sbergen <sbergen@cd1153cc-aba8-4b8a-9c6f-7f39c3b9f640>
2009-03-14 16:35:13 +0000

    ThreadSafe interfaces

--- trunk/Boids/src/boid/BoidState.java
+++ trunk/Boids/src/boid/BoidState.java
@@ -1,6 +1,16 @@
 package boid;
 
-public final class BoidState extends ConstBoidState {
+import java.util.ArrayList;
+
+import vector.Angle;
+import vector.Vector;
+
+public final class BoidState implements ThreadSafeBoidState {
+	
+	private BoidModel boid;
+	private PhysState state;
+	private PhysState nextState;
+	private static BoidList list;
 	
 	static void setList(BoidList newList) {
 		list = newList;
@@ -10,6 +20,65 @@ public final class BoidState extends ConstBoidState {
 		
 	}
 
+	public Vector getPosition() {
+		return state.position;
+	}
+
+	public Angle getAngle() {
+		return new Angle(state.speed);
+	}
+
+	public Vector getSpeed() {
+		return state.speed;
+	}
+	
+	public boolean hasLimits(engine.PhysLimits limits) {
+		return (boid.limits == limits);
+	}
+
+	public void calculateNextMove() {
+		Vector force = new Vector();
+		Vector separation = new Vector();
+		Vector cohesion = new Vector();
+		Vector alignment = new Vector();
+		
+		ArrayList<ThreadSafeBoidState> neighbours = 
+			list.getBoidsWithinRange(state.position, boid.limits.perceptionRange);
+		
+		for (ThreadSafeBoidState neighbour : neighbours) {
+			
+			/* Separation */
+			
+			Vector diff = new Vector(neighbour.getPosition());
+			diff.subtract(state.position);
+			diff.scale(1.0 / diff.length());
+			separation.add(diff);
+			
+			/* Cohesion and Alignment */
+			
+			cohesion.add(neighbour.getPosition());
+			alignment.add(neighbour.getSpeed());
+		}
+		
+		/* Cohesion finalization */
+		
+		cohesion.scale(1.0 / neighbours.size());
+		cohesion.subtract(state.position);
+		
+		/* Alignment finalization */
+		
+		alignment.scale(1.0 / neighbours.size());
+		alignment.subtract(state.speed);
+		
+		/* Force vector */
+		
+		force.add(separation.scale(0.2));
+		force.add(cohesion.scale(0.2));
+		force.add(alignment.scale(0.2));
+		
+		boid.attemptMove(state, nextState, force);
+	}
+	
 	void commitNextMove() {
 		PhysState tmp = state;
 		state = nextState;
